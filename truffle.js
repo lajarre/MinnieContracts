9c305285414a4c099f2330694c607dbdfb0fb5c5
Alexandre Hajjar <alexandre.hajjar@gmail.com>
2017-10-22 17:53:22 +0200

    Truffle deploy config

--- truffle.js
+++ truffle.js
@@ -3,7 +3,11 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      // gas: 5712388,
     }
-  }
+  },
+  deploy: [
+    "MinnieGovernance"
+  ]
 };
